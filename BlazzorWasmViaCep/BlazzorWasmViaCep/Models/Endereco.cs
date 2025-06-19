e60dca21c190f9ff8b2545b3ac3f30b2f6d2938d
Paiva <danpaiva@live.com>
2025-06-19 14:07:42 -0300

    feat: add files

---
+++ BlazzorWasmViaCep/BlazzorWasmViaCep/Models/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazzorWasmViaCep.Models;
+
+public class Endereco
+{
+    [JsonPropertyName("cep")]
+    public string Cep { get; set; }
+    [JsonPropertyName("logradouro")]
+    public string Rua { get; set; }
+    public string Complemento { get; set; }
+    public string Bairro { get; set; }
+    [JsonPropertyName("localidade")]
+    public string Cidade { get; set; }
+    public string Uf { get; set; }
+    public string Ibge { get; set; }
+    public string Gia { get; set; }
+    public string Ddd { get; set; }
+    public string Siafi { get; set; }
+}
